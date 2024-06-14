5aa6a224cb55d35b5f51685fd0a345091568b23e
nhutrung <nguyennhutrung.nnt@gmail.com>
2024-06-14 17:41:25 +0700

    fix text render

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 const subtitles = [
-  "Con người thích cười, điều này là rõ ràng và dễ hiểu",
-  "do đó con người sẽ tìm cách chọc cười nhau càng nhiều càng tốt",
+  "Con người thích cười",
+  "điều này là rõ ràng và dễ hiểu",
   "điều này đã trở nên quen thuộc và hiển nhiên",
-  "Thế nhưng có một điều mà vẫn còn ít người trong chúng ta quan tâm đến",
-  "đó là hậu quả gì sẽ xảy ra nếu chỉ vì mục đích được cười",
-  "liệu con người có sẵn sàng giải trí hoá mọi thứ trong xã hội này?",
 ];
 
 let currentIndex = 0;
-let currentLine = 1;
 let subtitleText = [];
 
-const line1 = document.getElementById("line1");
-const line2 = document.getElementById("line2");
+const line = document.getElementById("line");
+
+var i = 0;
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
 
 function showNextLetter() {
   if (currentIndex < subtitles.length) {
-    let currentSubtitle = subtitles[currentIndex].split(" ");
+    let currentSubtitle = subtitles[currentIndex].split("");
 
     if (subtitleText.length < currentSubtitle.length) {
       subtitleText = subtitleText.concat(currentSubtitle[subtitleText.length]);
-      if (currentLine === 1) {
-        line1.textContent = subtitleText.join(" ");
-      } else {
-        line2.textContent = subtitleText.join(" ");
-      }
-    } else {
-      line1.textContent = subtitleText.join(" ");
-      subtitleText = [];
-      currentLine = 2;
-      line2.textContent = "";
-      currentIndex++;
+      line.textContent = subtitleText.join("");
     }
-
-    setTimeout(showNextLetter, 200);
+  } else {
+    line.textContent = subtitleText.join("");
+    currentIndex++;
   }
+
+  setTimeout(showNextLetter, 50);
 }
 
 showNextLetter();
